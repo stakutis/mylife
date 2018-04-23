64207f5f9e74583e47db504e56ea9bdd3afb04ad
Chris Stakutis <chris.stakutis@gmail.com>
2018-04-23 19:14:41 +0000

    updated email

---
+++ mylife.js
@@ -0,0 +1,9 @@
+var AWS = require('aws-sdk');
+var db = new AWS.DynamoDB.DocumentClient(
+    {
+            region: 'us-east-1',
+            accessKeyId: 'AKIAJGWD2X3CMS3BKLRQ',
+            secretAccessKey: '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'});
+
+
+
